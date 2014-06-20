02d3bd165399bb3759b55471693ab52a851e47bf
kwinnik <kwinnik@.(none)>
2014-06-20 10:34:18 -0400

    added a global setter

--- Assets/GlobalGameObject/GlobalGameObject.cs
+++ Assets/GlobalGameObject/GlobalGameObject.cs
@@ -1,8 +1,4 @@
-﻿/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
- 
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 namespace GlobalGameObject
 {
@@ -17,6 +13,10 @@ namespace GlobalGameObject
 					_Instance = Create();
 				}
 				return _Instance;
+			}
+			set
+			{
+				_Instance = value;
 			}
 
 		}
---
+++ Assets/GlobalGameObject/GlobalGameObjectSetter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace GlobalGameObject
+{
+	public class GlobalGameObjectSetter : MonoBehaviour {
+
+		public MonoBehaviour GlobalComponent;
+		// Use this for initialization
+		void Awake () {
+
+			var d1 = typeof(GGO<>);
+			System.Type[] typeArgs = { GlobalComponent.GetType() };
+			var makeme = d1.MakeGenericType(typeArgs);
+			System.Reflection.PropertyInfo pi = makeme.GetProperty("Instance");
+			pi.SetValue(null, GlobalComponent, null);
+		}
+
+	}
+}
